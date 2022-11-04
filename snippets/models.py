f0f91a7bacbcf9be28ab2508c82fafb2c85ea4c0
he1dj <ssookkraatt@gmail.com>
2022-11-05 00:45:20 +0600

    first_commit

---
+++ snippets/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
+
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(max_length=100, default='python')
+    style = models.CharField(max_length=100, default='friendly')
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
+
+    class Meta:
+        ordering = ['created']
+
+    def save(self, *args, **kwargs):    
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
